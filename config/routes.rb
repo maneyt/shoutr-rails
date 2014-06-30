702607bc3a6afabd3b8d5191a4b0e188b31e8592
Thomas Maney <thomasjmaney@gmail.com>
2014-06-30 13:34:36 -0400

    render shout partial on user#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require "monban/constraints/signed_in"
 
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
 
   resources :shouts, only: [:create]
 
